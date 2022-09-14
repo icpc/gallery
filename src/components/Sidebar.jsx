cc33b2fd4cfc8b4ddf2a20e4ae83c2b4e3ef3de9
masha237 <zhogowamasha@gmail.com>
2022-09-14 23:40:05 +0300

    fix search

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,8 @@ import TableOfContents from "./TableOfContents";
 
 const Sidebar = ({setSearchParams}) => {
     return (
-        <div style={{width: "25%", marginLeft: "40px", marginTop: "20px"}}>
-            <img className={"logo"} src={process.env.PUBLIC_URL + "/logo.svg"} style={{width: "100px"}}/>
+        <div style={{width: "25%", marginLeft: "40px", marginTop: "50px"}}>
+            <img className={"logo"} src={process.env.PUBLIC_URL + "/icpc_header.svg"} style={{height: "63px"}}/>
             <TableOfContents setSearchParams={setSearchParams}/>
         </div>
     );
