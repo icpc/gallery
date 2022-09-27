0dba36b246165d3380f3fe26c42ffca8aa505b8f
masha237 <zhogowamasha@gmail.com>
2022-09-27 21:20:00 +0300

    fix logo and scroll

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import TableOfContents from "./TableOfContents";
+import {LAST_YEAR} from "../consts";
 
 const Sidebar = ({setSearchParams}) => {
     return (
-        <div style={{width: "25%", marginLeft: "40px", marginTop: "50px"}}>
-            <img className={"logo"} src={process.env.PUBLIC_URL + "/icpc_header.svg"} style={{height: "63px"}}/>
+        <div className={"sidebar"} style={{width: "25%", marginLeft: "40px", marginTop: "50px"}}>
+            <a href={"?album=" + LAST_YEAR}> <img className={"logo"} src={process.env.PUBLIC_URL + "/icpc_header.svg"} style={{height: "63px"}} alt={"go home page"}/></a>
             <TableOfContents setSearchParams={setSearchParams}/>
         </div>
     );
