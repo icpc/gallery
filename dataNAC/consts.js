b212fcd52551e564a5146780edd20bf0e0dfd30f
Lidia Perovskaya <lidia@icpc.global>
2023-02-15 14:29:37 +0300

    gallery nac consts

---
+++ dataNAC/consts.js
@@ -0,0 +1,30 @@
+export const years = [
+    "2022", "2021", "2020"
+];
+export const places =
+    {
+        "2022": "Orlando, FL",
+        "2021": "Online",
+        "2020": "Atlanta, GA"
+    }
+;
+
+
+export const api_key = "d30033b3d833adaace90f8487da70bba";
+export const user_id = "141939107@N06";
+
+export const TAG_EVENT = "event";
+export const TAG_TEAM = "team";
+export const TAG_ALBUM = "albumNAC";
+
+export const SVG_WIDTH = 60;
+export const SVG_HEIGHT = 60;
+
+export const PER_PAGE = 25;
+export const LAST_YEAR = "2022";
+export const CONTEST_NAME = "NAC";
+export const DEFAULT_EVENT = "Photo Tour";
+
+export const DEBUG = process.env.NODE_ENV === "development";
+
+
