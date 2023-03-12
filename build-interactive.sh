6c6b650c5fe0085fe9c96d8e77d6341935eafc56
Maxim <me@alzhanov.ru>
2023-03-12 22:55:07 +0500

    Fix release

--- build-interactive.sh
+++ build-interactive.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+read -p "Enter the public url for the application without '/' (default: gallery): " PUBLIC_URL
 PUBLIC_URL=${PUBLIC_URL:-gallery}
 
+read -p "Enter the data folder for the application (default: data): " VITE_DATA_FOLDER
 VITE_DATA_FOLDER=${VITE_DATA_FOLDER:-data}
 
 echo "Making bundle with public_url=/$PUBLIC_URL and data=$VITE_DATA_FOLDER"
