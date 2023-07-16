08a19adda9826008d06f74bbb0e495ad281723f0
Azat Ismagilov <azatismagilov00@gmail.com>
2023-07-16 05:00:02 +0200

    Make face locations correct, fix #4

--- src/components/Body/Lightbox.jsx
+++ src/components/Body/Lightbox.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "../../styles/Body.css"
 import PhotoInfo from "./PhotoInfo";
 import FaceDiv from "./FaceDiv";
@@ -6,17 +6,17 @@ import Control from "./SlideShowControl";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const Lightbox = ({
-                      isSlideShow,
-                      setIsSlideShow,
-                      photo,
-                      setPhoto,
-                      handelRotationLeft,
-                      handelRotationRight,
-                      leftArrow,
-                      rightArrow,
-                      photoInfo,
-                      setPhotoInfo
-                  }) => {
+    isSlideShow,
+    setIsSlideShow,
+    photo,
+    setPhoto,
+    handelRotationLeft,
+    handelRotationRight,
+    leftArrow,
+    rightArrow,
+    photoInfo,
+    setPhotoInfo
+}) => {
     const handelClick = (e) => {
         if (e.target.classList.contains("dismiss")) {
             setPhoto(null);
@@ -44,7 +44,7 @@ const Lightbox = ({
     const handleTouchMove = (e) => {
         const touchDown = touchPosition
 
-        if(touchDown === null) {
+        if (touchDown === null) {
             return
         }
 
@@ -64,23 +64,25 @@ const Lightbox = ({
     return (
         <div className="dismiss" onClick={handelClick} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
             <div className="wrapper">
+                <div>
+                    <img
+                        className="full"
+                        src={photo.url}
+                        alt={"fullsize"}
+                        onLoad={() => setIsLoaded(true)}
+                    />
+                    {photoInfo?.person?.map(person => (<FaceDiv person={person} face={face} setFace={setFace} key={person.name + "facediv" + person.position.top} />))}
+                </div>
                 <Control handelClick={handelClick} slideShow={slideShow} isSlideShow={isSlideShow}
-                         setPhoto={setPhoto}
-                         setIsSlideShow={setIsSlideShow}
-                         setPhotoInfo={setPhotoInfo}/>
-                <img
-                    className="full"
-                    src={photo.url}
-                    alt={"fullsize"}
-                    onLoad={() => setIsLoaded(true)}
-                />
-                <PhotoInfo photo={photo} photoInfo={photoInfo} setFace={setFace}/>
-                {photoInfo?.person?.map(person => (<FaceDiv person={person} face={face} setFace={setFace} key={person.name + "facediv" + person.position.top}/>))}
+                    setPhoto={setPhoto}
+                    setIsSlideShow={setIsSlideShow}
+                    setPhotoInfo={setPhotoInfo} />
+                <PhotoInfo photo={photo} photoInfo={photoInfo} setFace={setFace} />
                 {leftArrow && <div onClick={handelRotationLeft} className="overlay-arrows_left">
-                    <ArrowForwardIosIcon style={{transform: "rotate(180deg)"}}/>
+                    <ArrowForwardIosIcon style={{ transform: "rotate(180deg)" }} />
                 </div>}
                 {rightArrow && <div onClick={handelRotationRight} className="overlay-arrows_right">
-                    <ArrowForwardIosIcon/>
+                    <ArrowForwardIosIcon />
                 </div>}
             </div>
 
