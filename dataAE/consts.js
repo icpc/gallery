99e1dca9311082800eb4d32bc9c4d47284c6ac04
Lidia in NA <lidia@icpc.global>
2023-12-25 12:30:04 +0300

    moar galleries

---
+++ dataAE/consts.js
@@ -0,0 +1,32 @@
+export const years = [
+    "RC 2023-2024", "2023"
+];
+export const places =
+    {
+        "RC 2023-2024": "Regional Contests",
+        "2023": ""
+    }
+;
+
+
+export const api_key = "d30033b3d833adaace90f8487da70bba";
+export const user_id = "141939107@N06";
+export const title = "Asia East Gallery";
+export const description = "Asia East Photo Gallery.";
+
+export const FLICKR_IMAGE_PREFIX = "https://www.flickr.com/photos/icpcnews/";
+export const SUGGESTIONS_EMAIL = "lidia+galleryAE@icpc.global";
+
+export const TAG_EVENT = "event";
+export const TAG_TEAM = "team";
+export const TAG_ALBUM = "albumAE";
+export const TAG_PERSON = "person";
+export const TAG_PHOTOGRAPHER = "photographer";
+
+export const SVG_WIDTH = 60;
+export const SVG_HEIGHT = 60;
+
+export const PER_PAGE = 25;
+export const LAST_YEAR = "2023";
+export const CONTEST_NAME = "";
+export const DEFAULT_EVENT = "Photo Tour";
