c638d0d2486b0a5967798c867bc1283ad5d5f65e
Azat Ismagilov <azatismagilov00@gmail.com>
2024-08-02 22:29:11 +0100

    chore: Update PhotoService parameters for better sorting
    - Add sort=date-taken-desc to the parameters in PhotoService to ensure photos are sorted by date taken in descending order.

--- src/Util/PhotoService.js
+++ src/Util/PhotoService.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import { api_key, PER_PAGE, TAG_ALBUM, TAG_EVENT, TAG_TEAM, user_id } from "../consts";
 
 export default class PhotoService {
+    // Parameters help: https://www.flickr.com/services/api/flickr.photos.search.html
     static extras = "tags,machine_tags,url_m,url_c,url_l,url_o,description,date_upload,date_taken";
-    static parameters = `&per_page=${PER_PAGE}&extras=${PhotoService.extras}&format=json&nojsoncallback=?`;
+    static parameters = `&sort=date-taken-desc&per_page=${PER_PAGE}&extras=${PhotoService.extras}&format=json&nojsoncallback=?`;
 
     static getAllWithEvent(year, event = "Photo%20Tour", page = 1, config = {}) {
         return this.getAll(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${api_key}&user_id=${user_id}&tags=${TAG_EVENT}$${event},${TAG_ALBUM}$${year}&tag_mode=all&page=${page}` + PhotoService.parameters, config);
@@ -22,7 +23,7 @@ export default class PhotoService {
         return PhotoService.getAll(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${api_key}&user_id=${user_id}&page=${page}&text=${text}%20and%20${TAG_ALBUM}$` + PhotoService.parameters, config);
     }
 
-    static getPhotoInfo(id, config={}) {
+    static getPhotoInfo(id, config = {}) {
         return PhotoService.getAll(`https://api.flickr.com/services/rest?method=flickr.photos.getInfo&api_key=${api_key}&user_id=${user_id}&format=json&nojsoncallback=?&photo_id=${id}`, config);
     }
 
