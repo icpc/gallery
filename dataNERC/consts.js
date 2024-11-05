c2e86095f4d4347206231b4fd3bf701deb513551
Victoria Volochay <vvolochay@gmail.com>
2024-11-05 18:33:07 +0300

    nerc 24-25

--- dataNERC/consts.js
+++ dataNERC/consts.js
@@ -1,4 +1,5 @@
 export const places = [
+  ["RC 2024-2025", "Regional Contests", ""],
   ["2023", "SPb, Novosibirsk, Astana, Kutaisi", ""],
   ["RC 2023-2024", "Regional Contests", ""],
   ["2022", "SPb, Tinkoff Arena", ""],
