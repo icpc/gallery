6ec5e4865f20392f43dd7a8ee6e37b460390c2f2
Azat Ismagilov <azatismagilov00@gmail.com>
2025-06-04 00:18:00 +0100

    Update build script to include staging mode in build process

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ PUBLIC_URL=${PUBLIC_URL:-gallery}
 
 VITE_DATA_FOLDER=${VITE_DATA_FOLDER:-data}
 
+MODE=${MODE:-production}
+
 echo "Making bundle with public_url=/$PUBLIC_URL and data=$VITE_DATA_FOLDER"
 
-PUBLIC_URL="$PUBLIC_URL" VITE_DATA_FOLDER="$VITE_DATA_FOLDER" npm run build -- --base=/${PUBLIC_URL} || exit 1;
+PUBLIC_URL="$PUBLIC_URL" VITE_DATA_FOLDER="$VITE_DATA_FOLDER" npm run build -- --base=/${PUBLIC_URL} --mode=${MODE} || exit 1;
 
 zip -r $PUBLIC_URL.zip $PUBLIC_URL/* || exit 1;
 
