7ad88d725d6d6ded24c001b9c09efad68c99f55c
Azat Ismagilov <azatismagilov00@gmail.com>
2025-06-07 00:19:00 +0100

    chore: update devDependencies for eslint, typescript-eslint, and add @eslint/js

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { ignores: ["pocketbase/**"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+]);
