ac96f88585e90bda6d1570a19ea4732550818fa4
Azat Ismagilov <azatismagilov00@gmail.com>
2025-06-09 00:06:43 +0100

    feat: add @tanstack/react-query and @tanstack/eslint-plugin-query dependencies to package.json and package-lock.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from "@eslint/js";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginReact from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
@@ -9,6 +10,7 @@ export default tseslint.config(
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
   eslintConfigPrettier,
+  tanstackQuery.configs["flat/recommended"],
   {
     ignores: ["**/dist/**", "**/tests/**", "**/lib/**"],
   },
