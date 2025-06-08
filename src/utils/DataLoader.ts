8118e16fb3265b7b185ab7c48657dd7a7df93929
Azat Ismagilov <azatismagilov00@gmail.com>
2025-06-08 22:08:29 +0100

    refactor: remove DataLoader.ts as it contained unused imports and local definitions

--- src/utils/DataLoader.ts
+++
@@ -1,5 +0,0 @@
-import { getEventData, getPeopleData, getTeamData } from "../consts";
-
-// Removed unused imports and local definitions, now only importing from consts
-
-export { getEventData, getPeopleData, getTeamData };
