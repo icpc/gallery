2d080f315e61c2ef6033facf4cd100b62cb5316a
Azat Ismagilov <azatismagilov00@gmail.com>
2025-06-09 01:37:59 +0100

    fix: update garbage collection time to 7 days for query client

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 5 * 60 * 1000, // 5 minutes
-      gcTime: 24 * 60 * 60 * 1000, // 24 hours
+      gcTime: 7 * 24 * 60 * 60 * 1000, // 7 days
     },
   },
 });
