573178fc615a7e37ce3087a191a31aed79c004bf
Azat Ismagilov <azatismagilov00@gmail.com>
2025-06-16 23:44:34 +0100

    Refactor vite.config.mjs to use loadEnv for environment variables and improve code structure

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,49 +1,56 @@
-/* eslint-disable no-undef */
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "path";
+import { cwd } from "process";
+import { defineConfig, loadEnv } from "vite";
 
 import { robotsTxtGenerator } from "./src/plugins/robotsTxtGenerator.ts";
 import { sitemapGenerator } from "./src/plugins/sitemapGenerator.ts";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const baseUrl = "https://news.icpc.global";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    sitemapGenerator({
-      baseUrl,
-      publicUrl: process.env.PUBLIC_URL || "/",
-    }),
-    robotsTxtGenerator({
-      baseUrl,
-      publicUrl: process.env.PUBLIC_URL || "/",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-      "@data": resolve(__dirname, process.env.VITE_DATA_FOLDER || "data"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      sitemapGenerator({
+        baseUrl,
+        publicUrl: env.PUBLIC_URL || "/",
+      }),
+      robotsTxtGenerator({
+        baseUrl,
+        publicUrl: env.PUBLIC_URL || "/",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "@data": resolve(__dirname, env.VITE_DATA_FOLDER || "data"),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-    emptyOutDir: true,
-    outDir: process.env.PUBLIC_URL,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("@tanstack")) {
-            return "tanstack";
-          }
-          if (id.includes("react")) {
-            return "react";
-          }
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: "esnext",
+      emptyOutDir: true,
+      outDir: env.PUBLIC_URL || "dist",
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes("@tanstack")) {
+              return "tanstack";
+            }
+            if (id.includes("react")) {
+              return "react";
+            }
+          },
         },
       },
     },
-  },
+  };
 });
