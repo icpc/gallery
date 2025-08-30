fb8b163081227d3d8118faf8b4842a50d8b22a05
Lidia Perovskaya <lperovskaya@gmail.com>
2025-08-30 20:23:47 +0400

    2025 push

--- data/consts.js
+++ data/consts.js
@@ -186,7 +186,7 @@ export const TAG_PHOTOGRAPHER = "photographer";
 export const SVG_WIDTH = 60;
 export const SVG_HEIGHT = 60;
 
-export const LAST_YEAR = "2024";
+export const LAST_YEAR = "2025";
 export const DEFAULT_EVENT = "Photo Tour";
 
 export const MAX_ALBUM_SIZE = 2000;
