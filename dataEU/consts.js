26db7d335281916127d3c39d502e85cd036aedd7
Lidia Perovskaya <lperovskaya@gmail.com>
2026-02-05 11:17:24 -0800

    default 2026

--- dataEU/consts.js
+++ dataEU/consts.js
@@ -69,6 +69,6 @@ export const TAG_PHOTOGRAPHER = "photographer";
 export const SVG_WIDTH = 60;
 export const SVG_HEIGHT = 60;
 
-export const LAST_YEAR = "2025";
+export const LAST_YEAR = "2026";
 export const DEFAULT_EVENT = "Photo Tour";
 export const MAX_ALBUM_SIZE = 2000;
